4580c7df50f05c925ede4d0b0dbaf831b9e18d55
arashbayat1 <65373140+dasharash@users.noreply.github.com>
2021-05-06 16:24:33 -0400

    Added Heatmap

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+#from espn_api.basketball import League
 from config import *
 import streamlit as st
 import pandas as pd
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 # Style
@@ -16,41 +19,20 @@ st.markdown("""
 
 
 st.title('NBA Fantasy Statistics')
-
 st.markdown("""
 This web application executes webscraping of NBA player statistics and 
 transforms the data for easy viewing, analysis, and fantasy needs!\n
 **Try it out below!**
 """)
-
 st.sidebar.header('Display Filters')
+
+# Parameters
 year = st.sidebar.selectbox('Year', list(reversed(range(2000,2022))))
 fantasy = False
 filtered_teams = False
 scoring = False
-
 cats = []
 cats_weights = []
-
-def truncate(x):
-    end = str(int(x) % 100)
-    if (int (x) > 1000):
-        return (int(x) / 100)
-    elif (int(x) > 100):
-        return str(int(x) / 100)
-    return '0.' + end
-
-def truncy(x):
-    x *= 1000
-    if(x > 1000) :
-        x = str(x)
-        return float((x[:1]) + '.' + x[-2:])
-    if(x > 100) :
-        return x
-        x = str(x)
-        return float((x[0]) + '.' + x[-2:])
-    x = str(x)
-    return float((x[0]) + '.' + x[1:])
 
 @st.cache
 def load_teams():
@@ -67,14 +49,12 @@ if(filter_teams) :
 
 if(year >= 2020):
     fantasy = st.sidebar.checkbox('Fantasy')
-
 st.sidebar.markdown('<p class="small-font">*Fantasy stats only available for the last 2 years</p>', unsafe_allow_html=True)
-
 
 if(fantasy):
     scoring = st.sidebar.selectbox('Scoring Format', formats)
     cats = st.sidebar.multiselect('Categories', categories, default_fantasy)
-    i = 0
+
     if(scoring == 'H2H') :
         for i in range(len(cats)):
             cats_weights.append(st.sidebar.number_input(cats[i] + ' point',None,None,0.0,.5,'%f'))
@@ -97,7 +77,6 @@ def load_scrapped_data(year, fantasy, cats, positions, teams):
     for i in range(num_stat_start, len(data.columns)) :
         data.iloc[:,i] = data.iloc[:,i].apply(float)
 
-
     if (teams):
         data = data[data.Tm.isin(teams)]
     data = data[data.Pos.isin(positions)]
@@ -112,13 +91,26 @@ def load_scrapped_data(year, fantasy, cats, positions, teams):
             data['FT_PTS'] = total
             data = data.sort_values(by = ['FT_PTS'], ascending = False, ignore_index=True)
         style_format = {cats[0][i]: '{:.2f}' for i in range(len(cats[0]))}
-        return pd.DataFrame(data).style.format(style_format)
+        return [pd.DataFrame(data), style_format]
     
     data = data.reset_index(drop=True)
-    return pd.DataFrame(data).style.format(categories_style_dict)
+    return [pd.DataFrame(data),categories_style_dict]
 
 nbastats = load_scrapped_data(year, [fantasy, scoring], [cats, cats_weights], filtered_positions, filtered_teams)
 
+st.write(nbastats[0].style.format(nbastats[1]))
+
+heatmap = st.button('Intercorrelation Heatmap between Stats')
+st.markdown('<p class="small-font">Can be used to see the relationship between stats!</p>', unsafe_allow_html=True)
 
 
-st.write(nbastats)
+if heatmap:
+    st.header('Intercorrelation Heatmap')
+
+    correlations = nbastats[0].corr()
+    mask = np.zeros_like(correlations)
+    mask[np.triu_indices_from(mask)] = True
+    with sns.axes_style("darkgrid"):
+        fig, ax = plt.subplots(figsize=(7, 6))
+        ax = sns.heatmap(correlations, mask=mask, vmax=1, annot=True, linecolor="white" ,linewidths=.5,)
+    st.pyplot(fig)
